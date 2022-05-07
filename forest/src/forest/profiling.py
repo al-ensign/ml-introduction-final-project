d5148d9ef76527ecda80d7121afa7d9349f94d19
alensign <alexandra.wilchitskaya@gmail.com>
2022-05-07 13:17:24 +0300

    Update README file

---
+++ forest/src/forest/profiling.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from joblib import dump
+
+import click
+import pandas as pd
+import pandas_profiling
+
+from .data import get_data
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True
+)
+
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True
+
+def profile_report(
+    dataset_path: Path,
+    ) -> None:
+
+    data = get_data(dataset_path)
+    profile = data.profile_report(title="Forest Cover Type Prediction - Pandas Profiling Report")
+    profile.to_file("Forest_report.html")
